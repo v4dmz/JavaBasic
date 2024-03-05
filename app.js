6b976a35a73157b3eb498a13e34ae972a359a3b9
v4dmz <143401420+v4dmz@users.noreply.github.com>
2024-03-05 07:54:45 -0800

    JavaBasic

---
+++ app.js
@@ -0,0 +1,91 @@
+
+
+// if (false) {
+// }
+// console.log("Hello G");
+// if (true) {
+//   console.log("Yes Sir");
+// }
+
+var product = prompt("Enter product");
+
+if (product === "Broccoli") {
+  console.log("Available");
+}
+if (product !== "Broccoli") {
+  console.log("sorry only Mashrooms are available");
+}
+
+// logical operators
+
+// &&  and logical operator
+// ||  OR  logical  operator
+
+// console.log(true && true); // true
+// console.log(true && false); // false
+// console.log(false && true); // false
+// console.log(false && false); // false
+// console.log(true && false || false);
+// console.log(false || true && false);
+
+// var age = +prompt("Enter your age");
+
+// if (age <= 32) {
+//   console.log("The ticket price is 1200pkr");
+// }
+// if (age >= 40 && age < 49) {
+//   console.log("The ticket price is 1500pkr");
+// }
+// if (age >= 50) {
+//   console.log("They have free Entry ");
+// }
+
+// simple calculator
+
+// var valueOne = +prompt("Enter value one");
+// var valueTwo = +prompt("Enter value two");
+// var operator = prompt("Enter arithmetic operation");
+// if (
+//   operator === "+" ||
+//   operator === "-" ||
+//   operator === "*" ||
+//   operator === "/"
+// ) {
+//   console.log("conditional block is running");
+//   if (operator === "+") {
+//     console.log(valueOne + valueTwo);
+//   }
+//   if (operator === "-") {
+//     console.log(valueOne - valueTwo);
+//   }
+//   if (operator === "*") {
+//     console.log(valueOne * valueTwo);
+//   }
+//   if (operator === "/") {
+//     console.log(valueOne / valueTwo);
+//   }
+// } else {
+//   alert("invalid operator, please use + - * /");
+// }
+
+// Note: else  and else if must be used with if statement
+
+// if (
+//   operator === "+" ||
+//   operator === "-" ||
+//   operator === "*" ||
+//   operator === "/"
+// ) {
+//   console.log("conditional block is running");
+//   if (operator === "+") {
+//     console.log(valueOne + valueTwo);
+//   } else if (operator === "-") {
+//     console.log(valueOne - valueTwo);
+//   } else if (operator === "*") {
+//     console.log(valueOne * valueTwo);
+//   } else if (operator === "/") {
+//     console.log(valueOne / valueTwo);
+//   }
+// } else {
+//   alert("invalid operator, please use + - * /");
+// }
